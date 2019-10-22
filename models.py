47ace023e0af1763ddbe3e5da27d6ae0f9fd7f10
Bryan Li <bryanlimy@gmail.com>
2019-10-22 18:36:50 +0100

    update

--- models.py
+++ models.py
@@ -37,6 +37,6 @@ def get_discriminator(hparams):
       filters=256, kernel_size=3, strides=2, padding='causal')(outputs)
   outputs = tf.keras.layers.LeakyReLU(0.2)(outputs)
   outputs = tf.keras.layers.Flatten()(outputs)
-  outputs = tf.keras.layers.Dense(1, activation='sigmoid')(outputs)
+  outputs = tf.keras.layers.Dense(1)(outputs)
 
   return tf.keras.Model(inputs=inputs, outputs=outputs, name='discriminator')
