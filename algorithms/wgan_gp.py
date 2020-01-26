1bfb2475a2cc8f5728859327d3af2c92f57276cb
Bryan Li <bryanlimy@gmail.com>
2020-01-26 19:47:23 +0000

    update

--- algorithms/wgan_gp.py
+++ algorithms/wgan_gp.py
@@ -81,7 +81,7 @@ class WGAN_GP(GAN):
 
     return dis_loss, gradient_penalty
 
-  #@tf.function
+  @tf.function
   def train(self, inputs):
     dis_losses, gradient_penalties = [], []
     for i in range(self._n_critic):
