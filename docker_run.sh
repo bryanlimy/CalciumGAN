b4e3ac0ed22a5340e2897b02e1cf994c57725310
Bryan Li <bryanlimy@gmail.com>
2020-02-12 00:06:17 +0000

    update

--- docker_run.sh
+++ docker_run.sh
@@ -1,12 +1,16 @@
 #!/bin/bash
 
 function print_help() {
-  echo 'Usage: ./run_docker.sh [OPTIONS]'
-  echo 'Options:'
-  echo '  --gpu                     use GPUs'
+  echo "Usage: ./run_docker.sh --name [NAME] [OPTIONS]"
+  echo "  --name    name of the container"
+  echo "  --cpus    number of CPU cores to use"
+  echo "  --gpus     use GPUs"
+
 }
 
-use_gpu=false
+name='container_1'
+cpus=10
+gpus=false
 
 while [ ! $# -eq 0 ]; do
   case "$1" in
@@ -14,8 +18,16 @@ while [ ! $# -eq 0 ]; do
       print_help
       exit 1
       ;;
-    --gpu)
-      use_gpu=true
+    --name)
+      shift
+      name=$1
+      ;;
+    --cpus)
+      shift
+      cpus=$1
+      ;;
+    --gpus)
+      gpus=true
       ;;
     *)
       echo "Unknown flag $1, please check available flags with --help"
@@ -26,19 +38,24 @@ while [ ! $# -eq 0 ]; do
 done
 
 function main() {
-  if [ "$use_gpu" = "true" ]; then
-    docker run --gpus all -it --rm -u $(id -u):$(id -g) --cpus 12 \
-      -v ~/Git/calcium_imaging_gan/:/home/bryanlimy/calcium_imaging_gan \
-      -v /media/data0/bryanlimy/calcium_datasets/:/home/bryanlimy/calcium_imaging_gan/dataset/tfrecords \
-      -v /media/data0/bryanlimy/runs:/home/bryanlimy/calcium_imaging_gan/runs \
-      bryanlimy/projects:0.2-calcium-gan-base zsh
-  else
-    docker run -it --rm -u $(id -u):$(id -g) --cpus 12 \
-      -v ~/Git/calcium_imaging_gan/:/home/bryanlimy/calcium_imaging_gan \
-      -v /media/data0/bryanlimy/calcium_datasets/:/home/bryanlimy/calcium_imaging_gan/dataset/tfrecords \
-      -v /media/data0/bryanlimy/runs:/home/bryanlimy/calcium_imaging_gan/runs \
-      bryanlimy/projects:0.2-calcium-gan-base zsh
+  command="docker run -it --rm -u $(id -u):$(id -g)"
+  command+=" "
+  command+="--name $name"
+  command+=" "
+  command+="--cpus $cpus"
+  if [ "$gpus" = "true" ]; then
+    command+=" "
+    command+="--gpus all"
   fi
+  command+=" "
+  command+="-v ~/Git/calcium_imaging_gan/:/home/bryanlimy/calcium_imaging_gan"
+  command+=" "
+  command+="-v /media/data0/bryanlimy/calcium_datasets/:/home/bryanlimy/calcium_imaging_gan/dataset/tfrecords"
+  command+=" "
+  command+="-v /media/data0/bryanlimy/runs:/home/bryanlimy/calcium_imaging_gan/runs"
+  command+=" "
+  command+="bryanlimy/projects:0.2-calcium-gan-base zsh"
+  eval "$command"
 }
 
 main
