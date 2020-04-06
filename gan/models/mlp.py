6cb7ab11049a04c1bd74eb719b8a9e2150ffd2c0
Bryan Li <bryanlimy@gmail.com>
2020-04-06 22:29:00 +0800

    update fc and gru models

--- gan/models/mlp.py
+++ gan/models/mlp.py
@@ -4,7 +4,7 @@ import numpy as np
 import tensorflow as tf
 from tensorflow.keras import layers
 
-from .utils import activation_fn, calculate_input_config
+from .utils import activation_fn
 
 
 @register('mlp')
@@ -13,27 +13,30 @@ def get_models(hparams):
 
 
 def generator(hparams, units=64):
+  shape = (hparams.sequence_length, hparams.noise_dim)
+  noise_size = int(np.prod(shape))
+
   inputs = tf.keras.Input(shape=hparams.noise_shape, name='inputs')
 
-  shape, num_units = calculate_input_config(
-      output=hparams.sequence_length, noise_dim=hparams.noise_dim)
-
-  outputs = layers.Dense(num_units)(inputs)
+  outputs = layers.Dense(noise_size)(inputs)
   outputs = activation_fn(hparams.activation)(outputs)
   outputs = layers.Reshape(shape)(outputs)
 
+  # Layer 1
   outputs = layers.Dense(units)(outputs)
   if not hparams.no_batch_norm:
     outputs = layers.BatchNormalization()(outputs)
   outputs = activation_fn(hparams.activation)(outputs)
   outputs = layers.Dropout(hparams.dropout)(outputs)
 
+  # Layer 2
   outputs = layers.Dense(units * 2)(outputs)
   if not hparams.no_batch_norm:
     outputs = layers.BatchNormalization()(outputs)
   outputs = activation_fn(hparams.activation)(outputs)
   outputs = layers.Dropout(hparams.dropout)(outputs)
 
+  # Layer 3
   outputs = layers.Dense(units * 3)(outputs)
   if not hparams.no_batch_norm:
     outputs = layers.BatchNormalization()(outputs)
@@ -41,7 +44,6 @@ def generator(hparams, units=64):
   outputs = layers.Dropout(hparams.dropout)(outputs)
 
   outputs = layers.Dense(hparams.num_channels)(outputs)
-  outputs = layers.Reshape(hparams.signal_shape)(outputs)
 
   if hparams.normalize:
     outputs = activation_fn('sigmoid', dtype=tf.float32)(outputs)
@@ -54,18 +56,22 @@ def generator(hparams, units=64):
 def discriminator(hparams, units=64):
   inputs = tf.keras.Input(shape=hparams.signal_shape, name='inputs')
 
+  # Layer 1
   outputs = layers.Dense(units * 4)(inputs)
   outputs = activation_fn(hparams.activation)(outputs)
   outputs = layers.Dropout(hparams.dropout)(outputs)
 
+  # Layer 2
   outputs = layers.Dense(units * 3)(outputs)
   outputs = activation_fn(hparams.activation)(outputs)
   outputs = layers.Dropout(hparams.dropout)(outputs)
 
+  # Layer 3
   outputs = layers.Dense(units * 2)(outputs)
   outputs = activation_fn(hparams.activation)(outputs)
   outputs = layers.Dropout(hparams.dropout)(outputs)
 
+  # Layer 4
   outputs = layers.Dense(units)(outputs)
   outputs = activation_fn(hparams.activation)(outputs)
   outputs = layers.Dropout(hparams.dropout)(outputs)
