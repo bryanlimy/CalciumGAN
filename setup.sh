fe46f509e34acc5008536e8acdb3a6d30f4ba669
Bryan Li <bryanlimy@gmail.com>
2020-10-02 09:26:13 +0100

    update

--- setup.sh
+++ setup.sh
@@ -6,53 +6,51 @@ macOS=false
 check_requirements() {
   case "$(uname -s)" in
     Darwin)
-      echo 'Installing on macOS.'
+      printf 'Installing on macOS...'
       export CFLAGS='-stdlib=libc++'
       macOS=true
       ;;
     Linux)
-      echo 'Install on Linux.'
+      printf 'Installing on Linux...'
       ;;
     *)
-      echo 'Only Linux and macOS systems are currently supported.'
+      printf 'Only Linux and macOS systems are currently supported.'
       exit 1
       ;;
   esac
 }
 
 install_python_packages() {
-  echo 'Install tensorflow'
+  printf '\nInstalling tensorflow...'
   if [ "$macOS" = "true" ]; then
     python3 -m pip install tensorflow==2.2.0
   else
     conda install -c anaconda tensorflow-gpu==2.2.0 -y
   fi
-  echo 'Install Python packages'
+  printf '\nInstalling Python packages...'
   python3 -m pip install -r requirements.txt
 }
 
 install_oasis() {
-  echo 'Install OASIS'
+  printf '\nInstalling OASIS...'
   cd "$current_dir" || exit 1
-  echo 'Install Gurobi'
+  printf '\nInstalling Gurobi...'
   conda config --add channels http://conda.anaconda.org/gurobi
   conda install gurobi -y
-  echo 'Install Mosek'
+  printf '\nInstalling Mosek...'
   conda install -c mosek mosek -y
   git clone https://github.com/j-friedrich/OASIS.git oasis
   cd oasis || exit 1
   python3 setup.py build_ext --inplace
   python3 -m pip install -e .
-  echo 'Install OASIS completed'
 }
 
 install_elephant() {
-  echo 'Install Elephant'
+  printf '\nInstalling Elephant...'
   cd "$current_dir" || exit 1
   git clone git://github.com/NeuralEnsemble/elephant.git elephant
   cd elephant || exit 1
   python3 setup.py install
-  echo 'Install Elephant completed'
 }
 
 check_requirements
@@ -60,4 +58,4 @@ install_python_packages
 install_oasis
 install_elephant
 
-echo 'Setup completed.'
+printf '\nSetup completed.'
